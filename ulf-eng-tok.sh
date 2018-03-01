a8f3b1ef4ca0a4b2bd4bb44d21516bb76cc134c7
Thamme Gowda <tg@isi.edu>
2018-02-28 17:12:14 -0800

    make it work on OSX and python3

--- ulf-eng-tok.sh
+++ ulf-eng-tok.sh
@@ -5,5 +5,5 @@ cat $1 \
  | $DIR/wildeclean-v1.0.pl \
  | $DIR/normalize-punctuation.pl \
  | $DIR/v1.3.4/bin/tokenize-english.pl \
- | sed -u -e 's/ @@ / /g' \
+ | sed  -e 's/ @@ / /g' \
  | $DIR/utftest
