8ad5ca07e1d62453b68905f62010d88e66ead620
Thamme Gowda <tg@isi.edu>
2019-06-20 22:17:28 -0700

    disable sed -u bcoz OSX sed doesnt know it😣

--- ulf-eng-tok.sh
+++ ulf-eng-tok.sh
@@ -5,5 +5,5 @@ cat $1 \
  | $DIR/wildeclean-v1.0.pl \
  | $DIR/normalize-punctuation.pl \
  | $DIR/current/bin/tokenize-english.pl \
- | sed  -e 's/ @@ / /g' \
+ | sed -e 's/ @@ / /g' \
  | $DIR/utftest
--- ulf-src-tok.sh
+++ ulf-src-tok.sh
@@ -5,5 +5,5 @@ $DIR/wildeclean-v1.0.pl \
 | $DIR/normalize-punctuation.pl \
 | $DIR/utftest \
 | $DIR/current/bin/tokenize-english.pl \
-| sed -u -e 's/ @\([\\*:/-]\)/ \1/g' -e 's/\([\\*:/-]\)@ /\1 /g' -e 's/ @@ / /g' \
+| sed -e 's/ @\([\\*:/-]\)/ \1/g' -e 's/\([\\*:/-]\)@ /\1 /g' -e 's/ @@ / /g' \
 | $DIR/utftest
