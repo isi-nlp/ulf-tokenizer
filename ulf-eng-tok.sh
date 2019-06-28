2dcf60ed98af77bf53e5d5787ef85a181e1dde74
Thamme Gowda <tg@isi.edu>
2019-06-28 16:17:32 -0700

    inp out paths optional; STDIN STDOUT as defaults

--- ulf-eng-tok.sh
+++ ulf-eng-tok.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+[[ -n $1 ]] && inp=$1 || inp=/dev/stdin
+[[ -n $2 ]] && out=$2 || out=/dev/stdout
 
 DIR=$(dirname $BASH_SOURCE)
-cat $1 \
+cat $inp \
  | $DIR/wildeclean-v1.0.pl \
  | $DIR/normalize-punctuation.pl \
  | $DIR/current/bin/tokenize-english.pl \
  | sed  -e 's/ @@ / /g' \
- | $DIR/utftest
+ | $DIR/utftest > $out
--- ulf-src-tok.sh
+++ ulf-src-tok.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+[[ -n $1 ]] && inp=$1 || inp=/dev/stdin
+[[ -n $2 ]] && out=$2 || out=/dev/stdout
 
 DIR=$(dirname ${BASH_SOURCE[0]})
-$DIR/wildeclean-v1.0.pl \
+cat $inp \
+| $DIR/wildeclean-v1.0.pl \
 | $DIR/normalize-punctuation.pl \
 | $DIR/utftest \
 | $DIR/current/bin/tokenize-english.pl \
 | sed -u -e 's/ @\([\\*:/-]\)/ \1/g' -e 's/\([\\*:/-]\)@ /\1 /g' -e 's/ @@ / /g' \
-| $DIR/utftest
+| $DIR/utftest > $out
